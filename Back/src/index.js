14f66830ac4fb9c22ec19f1b71fd9acd6eb053f7
Skirmante Matiuke <skirmante.matiuke@>
2021-12-22 18:38:50 +0200

    completed database

--- Back/src/index.js
+++ Back/src/index.js
@@ -16,39 +16,50 @@ const main = async () => {
             port: MYSQL_PORT,
             user: MYSQL_USER,
             password: MYSQL_PW,
-            database: "ecommerce",
+            database: "users",
         });
 
-        const createUsersTable = `
-        CREATE TABLE IF NOT EXISTS Users 
+        const createBillsTable = `CREATE TABLE IF NOT EXISTS Bills ( 
+            id INTEGER NOT NULL AUTO_INCREMENT,
+            group_id INT NOT NULL,
+            description TEXT NOT NULL,
+            amount DECIMAL(3, 2) NOT NULL,
+            PRIMARY KEY (id)),
+            FOREIGN KEY (group_id) REFERENCES groups (id)`;
+            
+        const createUsersTable = `CREATE TABLE IF NOT EXISTS Users ( 
             id INTEGER NOT NULL AUTO_INCREMENT,
             full_name TEXT (20) NOT NULL,
             email VARCHAR (50) NOT NULL,
             password VARCHAR (50) NOT NULL,
-            REG_TIMESTAMP
+            REG_TIMESTAMP INTEGER,
+            PRIMARY KEY (id))`;
 
-            ,
-            PRIMARY KEY (id)`;
-
-        const creatBillsTable = `
-        CREATE TABLE IF NOT EXISTS Bills (
+        const createGroupsTable = `CREATE TABLE IF NOT EXISTS Groups (
             id INTEGER NOT NULL AUTO_INCREMENT,
-            product_id INTEGER NOT NULL,
-            customer_name VARCHAR (20) NOT NULL,
-            customer_email VARCHAR (50) NOT NULL,
-            PRIMARY KEY (id),
-            FOREIGN KEY (product_id) REFERENCES Products (id)`;
+            full_name TEXT (20) NOT NULL,
+            PRIMARY KEY (id))`;
 
+        const createAccountsTable = `CREATE TABLE IF NOT EXISTS Accounts ( 
+            id INTEGER NOT NULL AUTO_INCREMENT,
+            group_id INTEGER NOT NULL,
+            user_id INTEGER NOT NULL,
+            PRIMARY KEY (id);
+            FOREIGN KEY (group_id) REFERENCES groups (id),
+            FOREIGN KEY (user_id) REFERENCES users (id))`;
+                
         await connection.query(createUsersTable);
-        await connection.query(creatBillsTable);
-        
+        await connection.query(createBillsTable);
+        await connection.query(createGroupsTable);
+        await connection.query(createAccountsTable);
+                
         app.get("/", (req, res) => {
             res.send({
                 ip: req.ip,  
             });
         });
 
-        app.listen(5501, () => {
+        app.listen(8080, () => {
         console.log("http://localhost:8080");
         });
     } catch (error) {
